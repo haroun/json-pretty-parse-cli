ae0eda8d8943a4b1360e75e44294d64a0a9daf3a
Harouna Traore <h.traore@icloud.com>
2017-12-02 22:56:43 +0100

    add use cases

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 const test = require('tape')
 const execa = require('execa')
 
+const escape = string => JSON.stringify(string)
+
 test('json-pretty unescaped string', async assert => {
-  const message = 'should be "pretty", types should be kept'
+  const message = 'should return JSON object'
 
   const {stdout} = await execa(
     './cli.js',
@@ -27,23 +29,19 @@ test('json-pretty unescaped string', async assert => {
 })
 
 test('json-pretty escaped once string', async assert => {
-  const message = 'should be "pretty", types should be kept'
+  const message = 'should return JSON object'
 
   const {stdout} = await execa(
     './cli.js',
     [
-      '{\"number\":42,\"string\":\"this is a string obviously\",\"boolean\":true,\"array\":[1,2,4],\"object\":{\"a\":1},\"null\":null}'
+      escape('{"number":42,"string":"string"}')
     ]
   )
 
   const actual = stdout
   const expected = `{
     "number": 42,
-    "string": "this is a string obviously",
-    "boolean": true,
-    "array": [1, 2, 4],
-    "object": {"a": 1},
-    "null": null
+    "string": "string"
   }`
 
   assert.equal(actual, expected, message)
@@ -51,24 +49,84 @@ test('json-pretty escaped once string', async assert => {
   assert.end()
 })
 
-test('json-pretty escaped once string', async assert => {
-  const message = 'should be "pretty", types should be kept'
+test('json-pretty escaped twice string', async assert => {
+  const message = 'should return JSON object'
 
   const {stdout} = await execa(
     './cli.js',
     [
-      '{\\\"number\\\":42,\\\"string\\\":\\\"this is a string obviously\\\",\\\"boolean\\\":true,\\\"array\\\":[1,2,4],\\\"object\\\":{\\\"a\\\":1},\\\"null\\\":null}'
+      escape(escape('{"number":42,"string":"string"}'))
     ]
   )
 
   const actual = stdout
   const expected = `{
     "number": 42,
-    "string": "this is a string obviously",
-    "boolean": true,
-    "array": [1, 2, 4],
-    "object": {"a": 1},
-    "null": null
+    "string": "string"
+  }`
+
+  assert.equal(actual, expected, message)
+
+  assert.end()
+})
+
+test('json-pretty without spaces', async assert => {
+  const message = 'stdin should be transformed into JSON object'
+
+  const {stdout} = await execa('./cli.js', ['--space', 0], {input: '{"number":42,"string":"string"}'})
+
+  const actual = stdout
+  const expected = `{
+  "number": 42,
+  "string": "string"
+  }`
+
+  assert.equal(actual, expected, message)
+
+  assert.end()
+})
+
+test('json-pretty without spaces (alias)', async assert => {
+  const message = 'stdin should be transformed into JSON object'
+
+  const {stdout} = await execa('./cli.js', ['-s', 0], {input: '{"number":42,"string":"string"}'})
+
+  const actual = stdout
+  const expected = `{
+  "number": 42,
+  "string": "string"
+  }`
+
+  assert.equal(actual, expected, message)
+
+  assert.end()
+})
+
+test('json-pretty using stdin', async assert => {
+  const message = 'stdin should be transformed into JSON object'
+
+  const {stdout} = await execa('./cli.js', [], {input: '{"number":42,"string":"string"}'})
+
+  const actual = stdout
+  const expected = `{
+    "number": 42,
+    "string": "string"
+  }`
+
+  assert.equal(actual, expected, message)
+
+  assert.end()
+})
+
+test('json-pretty using stdin without spaces', async assert => {
+  const message = 'stdin should be transformed into JSON object'
+
+  const {stdout} = await execa('./cli.js', ['-s', 0], {input: '{"number":42,"string":"string"}'})
+
+  const actual = stdout
+  const expected = `{
+    "number": 42,
+    "string": "string"
   }`
 
   assert.equal(actual, expected, message)
